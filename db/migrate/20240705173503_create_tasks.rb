52be4df8632f58f06627fe18bdff8b75228513e3
Dana Zack <dzack@joinhomebase.com>
2024-07-09 08:59:27 -0600

    Feat: create active record association between tasks and categories

--- db/migrate/20240705173503_create_tasks.rb
+++ db/migrate/20240705173503_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[7.1]
     create_table :tasks do |t|
       t.string :title
       t.text :description
-      t.string :category
+      t.references :category, null: false, foreign_key: true
 
       t.timestamps
     end
